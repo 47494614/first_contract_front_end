beb43de92aa6cfb9fb9c48cc18cb697ff3ceb4f6
longxinpingxx <dxusr@outlook.com>
2024-12-03 14:22:49 +0800

    jetton.meta

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,20 @@ function App() {
     sendIncrement,
     sendDispot,
     sendWithdraw
+   
   } = useMainContract();
   const { connected } = useTonConnect();
+  const calcStoreFee = ()=>{
+    const bit_price=1;
+    const cell_price_ps=500;
+    const size = 1024*1024*8;
+    const time_delta = 3600*24*365;
+    const cell_count = Math.floor(size/1023);
+    const storage_fee = (cell_count * cell_price_ps + size * bit_price)
+    / 2**16 * time_delta;
+    return storage_fee;
+  };
+  const g_fee = calcStoreFee();
   return (
     <div>
       <div>
@@ -49,9 +61,10 @@ function App() {
           ) }
         </div>
         <div className="Card">
-          { connected && (
+          {
+            connected && (
             <a onClick={()=>{sendDispot(0.01);}}>
-              charge 0.01
+              charge 0.01 {g_fee}
             </a>
           ) }
         </div>
